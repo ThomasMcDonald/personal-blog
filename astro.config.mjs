14aba10647c2fe81396446f1e54f679d4355fcdc
Thomas McDonald <tom@fullstop.ai>
2024-11-13 17:04:11 +1000

    Added github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://thomasmcdonald.github.io",
+  base: "/personal-blog",
   integrations: [mdx(), sitemap(), tailwind()],
 });
