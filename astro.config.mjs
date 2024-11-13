61c71f6261ad082c971fee0e9bc111882d91fe6b
ThomasMcDonald <thomasmcdonald1996@gmail.com>
2024-11-13 18:24:09 +1000

    Added prod check to base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://thomasmcdonald.github.io/personal-blog",
-  base: "/",
+  site: "https://thomasmcdonald.github.io",
+  base: import.meta.env.PROD ? "/personal-blog" : "/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
